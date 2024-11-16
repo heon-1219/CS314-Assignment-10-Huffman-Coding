31032b06cd040f2c5f0df6cc91737544be02e0fb
vjm295 <deathcap23@gmail.com>
2024-11-15 19:14:13 -0600

    Priority queue added

--- PriorityQueue314.java
+++ PriorityQueue314.java
@@ -1,2 +1,53 @@
-public class PriorityQueue314 {
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class PriorityQueue314<E extends Comparable<? super E>>  {
+
+    private LinkedList<E> list;
+    private int size;
+    private int priority;
+
+    public PriorityQueue314() {
+        list = new LinkedList<>();
+    }
+
+    public void enqueue(E e) {
+        if (e == null) {
+            throw new NullPointerException();
+        }
+        list.add(e);
+        Collections.sort(list);
+    }
+
+    public E poll() {
+        if (list.isEmpty()) {
+            return null;
+        }
+        Collections.sort(list);
+        return list.remove(0);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public E peek() {
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.peek();
+    }
+
+    public boolean contains(E e) {
+        return list.contains(e);
+    }
+
+    public Iterator<E> iterator() {
+        return list.iterator();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
 }
